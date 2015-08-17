3442cf512040f3c6e612ed7dfe0f7c487b6ee1db
darkrpd <aliaskin@outlook.com>
2015-08-17 15:33:19 +0300

    v0.2

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,8 @@
 class Topic < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
   belongs_to :user
   has_many :posts
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/edit_profile', to: "users#edit_profile"
+
   resources :topics do
     resources :posts
   end
